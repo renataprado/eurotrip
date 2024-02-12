e34f7a92ffb77853ba321c55f8c88028dc4ae04c
renaskigi <dev.renataprado@gmail.com>
2024-02-12 20:54:34 -0300

    first commiy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './home/Home';
-import CountdownTimer from './countdown/CountDown';
-import CountDown2 from './countdown/CountDown2';
+import "./App.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Home from "./home/Home";
+import Documents from "./documents/Documents";
 
 const App = () => {
   return (
-    <div className="App">
-      <CountdownTimer />
-      <CountDown2 />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="App">
+              <Home />
+            </div>
+          }
+        />
+        <Route path="/documents" element={<Documents />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
