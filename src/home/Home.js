18dc260a6d253bcc3beb9dcc4176a47a9ff1f29c
renaskigi <dev.renataprado@gmail.com>
2024-02-21 02:45:00 -0300

    add gif map and fireworks

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,17 +1,28 @@
 import huhu from "./huhu.gif";
+import map from "./map0.gif"
 import CountdownTimer from "../countdown/CountDown";
+import { useEffect } from "react";
 import Button from '@mui/material/Button';
+
+import { Fireworks } from 'fireworks-js'
+
 
 
 const Home = () => {
+  useEffect(() => {
+    const container = document.querySelector('.container');
+    const fireworks = new Fireworks(container);
+    fireworks.start();
+  }, []);
+
   return (
     <div className="home">
       <h2>EUROTRIP PORRA</h2>
       <div className="countdown-container">
         <CountdownTimer />
-        <a href="/eurotrip/documents" >
-          <img src={huhu} alt="huhu"/>
-        </a>
+        <div className="container"></div>
+        <img src={huhu} alt="huhu"/>
+        <img src={map} alt="map"/>
       </div>
     </div>
   );
